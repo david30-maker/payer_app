c79a5595728114f02a8c26912a41c739ccc02901
david30-maker <davidokpah396@gmail.com>
2023-08-26 20:13:50 +0100

    ADD:implement for new player

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,6 +2,9 @@ class PlayersController < ApplicationController
     def index
         @players = Player.all
     end
+    def new
+        # @player = Player.new
+    end
     
     # def show
     #     @player = Player.find_by(id: params[:id])
