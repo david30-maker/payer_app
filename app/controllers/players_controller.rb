124928b2505e4caa5ce16392f646088e3fcbb070
david30-maker <davidokpah396@gmail.com>
2023-08-30 00:35:42 +0100

    ADD:implement delete routes

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -23,6 +23,23 @@ class PlayersController < ApplicationController
     def edit
         @player = Player.find(params[:id])
     end
+
+    def update
+        player = Player.find(params[:id])
+        if player.update(player_params)
+            redirect_to "/players"
+        else
+            flash[:errors] = player.errors.full_messages
+            redirect_to "/players/#{player.id}/edit"
+        end
+    end
+
+    def destroy
+        player = Player.find(params[:id])
+        player.destroy
+        redirect_to players_path, notice: 'Player was successfully deleted'
+    end
+
     private
         def player_params
             params.require(:player).permit(:first_name, :last_name)
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ get "players/new" => "players#new"
 post "players" => "players#create"
 get "players/:id" => "players#show"
 get "players/:id/edit" => "players#edit"
+patch "players/:id" => "players#update"
+delete "players/:id" => "players#destroy"
 end
